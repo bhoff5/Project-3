8f0d92f9dfef27ea3b6773d7ac8e6712c3325709
Colin <viewtifuljho@hotmail.com>
2019-10-24 20:33:39 -0500

    modified bill and household models, updated seeds

--- models/household.js
+++ models/household.js
@@ -1,7 +1,9 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
+const ObjectId = mongoose.Schema.Types.ObjectId;
 
 const householdSchema = new Schema({
+  _id: {type: ObjectId },
   name: { type: String, required: true },
   dateCreated: { type: Date, default: Date.now },
   tenants: [String]
