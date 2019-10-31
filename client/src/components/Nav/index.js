d5e869efdece9e9d78400e5403e921cc619f6ef0
Paul David Lampe <pdlampe@gmail.com>
2019-10-30 22:50:51 -0500

    Updated Navbar with final design

--- client/src/components/Nav/index.js
+++ client/src/components/Nav/index.js
@@ -1,27 +1,30 @@
 
 import React, { Component } from "react";
 import "./style.css";
-
+import Logo from "./apartgandw.png";
 
 class Nav extends Component {
   render() {
     return (
+
+
       <div>
         <nav>
           <div className="nav-wrapper green accent-4">
-            <a href="/" className="brand-logo">
-              Logo
+            <a href="/" className="brand-logo right">
+              <img src={Logo} alt="website logo" />
             </a>
-            <ul id="nav-mobile" className="right hide-on-med-and-down">
+            <ul id="nav-mobile" className="left">
               <li>
-                <a href="sass.html">Sass</a>
+                <a href="sass.html" class="green-text text-darken-2 green lighten-5 waves-effect waves-light btn">Your Household</a>
               </li>
               <li>
-                <a href="badges.html">Components</a>
+                <a href="badges.html" class="green-text text-darken-2 green lighten-5 waves-effect waves-light btn">Invite</a>
               </li>
               <li>
-                <a href="collapsible.html">JavaScript</a>
+                <a href="collapsible.html" class="green-text text-darken-2 green lighten-5 waves-effect waves-light btn">Log Out</a>
               </li>
+
             </ul>
           </div>
         </nav>
