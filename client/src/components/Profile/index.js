2c4093992a88e7cd4d546b26c7033462299de8c3
Shannon <shannonburke.art@gmail.com>
2019-11-01 23:48:50 -0500

    user profile skeleton

--- client/src/components/Profile/index.js
+++ client/src/components/Profile/index.js
@@ -1,21 +1,59 @@
 import React, { Component } from "react";
 import "./style.css";
 
-class Profile extends Component {
+class ProfileInfo extends Component {
     render() {
         return (
             <Container fluid>
                 {/* should display username, associated email, associated households and household validation code. additional bonus of "invite a new user" button. Also bonus: "create a new household" */}
 
                 <div class="row">
-                    <div class="card">
-
+                    <div class="card" id="basic info">
+                        <div class="card-title">
+                            Your info
+                        </div>
+                        <div class="card-content">
+                            <span>Your email address:</span><span id="userEmail"></span>
+                            <br />
+                            <span>Your display name:</span><span id="userId"></span>
+                            <br />
+                        </div>
                     </div>
                 </div>
             </Container>
         );
     }
-}
+};
+
+class ProfileHouseholds extends Component {
+    render() {
+        return (
+            <Container fluid>
+                {/* should display username, associated email, associated households and household validation code. additional bonus of "invite a new user" button. Also bonus: "create a new household" */}
+
+                <div class="row">
+                    <div class="card" id="householdsList">
+                        <div class="card-title">
+                            Your info
+                        </div>
+                        <div class="card-content">
+                            <span>Household ID:</span><span class="householdId"></span>
+                            <br />
+                        </div>
+                        <div class="card-action">
+                            <button id="invite-user">Invite</button>
+                        </div>
+                        <div class="card-action">
+                            <button id="create-new">Create new household</button>
+                        </div>
+                    </div>
+                </div>
+            </Container>
+        );
+    }
+};
 
 
-export default Profile;
+
+export default ProfileInfo;
+export default ProfileHouseholds;
