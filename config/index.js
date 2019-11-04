9ba41e6c34d8776e197655e9c367bc59c19cb8d9
Brian Hoff <blhoff52002@gmail.com>
2019-11-04 14:11:53 -0600

    updating config file to deploy to heroku

--- config/index.js
+++ config/index.js
@@ -1,15 +1,17 @@
 const mongoose = require("mongoose");
-mongoose.Promise = global.Promise
+mongoose.Promise = global.Promise;
 
-const uri = process.env.MONGODB_URI || "mongodb://localhost/apartdb";
+const uri =
+  process.env.MONGODB_URI ||
+  "mongodb://user:apart1@ds141198.mlab.com:41198/heroku_9q7wwp10";
 
 mongoose.connect(uri).then(
-    () => {
-        console.log("Connected to Mongo");
-    },
-    err => {
-        console.log(err)
-    }
+  () => {
+    console.log("Connected to Mongo");
+  },
+  err => {
+    console.log(err);
+  }
 );
 
 module.exports = mongoose.connection;
