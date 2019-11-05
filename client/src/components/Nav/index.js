abd420575d94b1d9e4ba6e2163e68a978da84e9f
Brian Hoff <blhoff52002@gmail.com>
2019-11-05 11:27:08 -0600

    addink link components to nav

--- client/src/components/Nav/index.js
+++ client/src/components/Nav/index.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import "./style.css";
 import Logo from "./apartgandw.png";
+import { Link } from "react-router-dom";
 
 class Nav extends Component {
   render() {
@@ -13,30 +14,46 @@ class Nav extends Component {
             </a>
             <ul id="nav-mobile" className="left">
               <li>
-                <a
+                <Link to="/">
+                  <button className="green-text text-darken-2 green lighten-5 waves-effect waves-light btn">
+                    Home
+                  </button>{" "}
+                </Link>
+                {/* <a
                   href="/"
                   className="green-text text-darken-2 green lighten-5 waves-effect waves-light btn"
                 >
                   Home
-                </a>
+                </a> */}
               </li>
               <li>
-                <a
+                <Link to="/signup">
+                  <button className="green-text text-darken-2 green lighten-5 waves-effect waves-light btn">
+                    Sign Up
+                  </button>{" "}
+                </Link>
+                {/* <a
                   href="/signup"
                   className="green-text text-darken-2 green lighten-5 waves-effect waves-light btn"
                 >
                   Invite
-                </a>
+                </a> */}
               </li>
               <li>
-                <a
+                <Link to="/newbill">
+                  <button className="green-text text-darken-2 green lighten-5 waves-effect waves-light btn">
+                    Create Bill
+                  </button>{" "}
+                </Link>
+
+                {/* <a
 
                   href="/newbill"
 
                   className="green-text text-darken-2 green lighten-5 waves-effect waves-light btn"
                 >
                   New Bill
-                </a>
+                </a> */}
               </li>
               <li>
                 <a
