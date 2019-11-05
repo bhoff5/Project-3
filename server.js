05ea6c9ba854171f137941d46c5335955ac20754
Brian Hoff <blhoff52002@gmail.com>
2019-11-05 11:50:57 -0600

    adding app.get to server.js

--- server.js
+++ server.js
@@ -53,6 +53,10 @@ if (process.env.NODE_ENV === "production") {
 
 app.use(routes);
 
+app.get("/*", function(req, res) {
+  res.sendFile(path.join(__dirname, "build", "index.html"));
+});
+
 app.use(
   session({
     secret: "additional-pylons",
