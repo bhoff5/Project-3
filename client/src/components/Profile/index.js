8ed3483f6ffc1ed71e78943eccdc845fb0296fd9
Shannon <shannonburke.art@gmail.com>
2019-11-04 23:51:49 -0600

    working on user profile page

--- client/src/components/Profile/index.js
+++ client/src/components/Profile/index.js
@@ -7,35 +7,20 @@ export function Profile(props) {
     console.log(props.displayName);
     return (
         <div>
-            {/* should display username, associated email, associated households and household validation code. additional bonus of "invite a new user" button. Also bonus: "create a new household" */}
+            {/* should display username, display name, andassociated email */}
 
             <div class="row">
-                <div class="card" id="basic info">
+                <div class="card" id="basic-info">
                     <div class="card-title">
                         Your info
                         </div>
                     <div class="card-content">
-                        <span>Your email address:</span><span id="userEmail"></span>
+                        <span>Your email address:{props.emailAddress}</span>
                         <br />
-                        <span>Your display name:</span>{props.displayName}<span id="userId"></span>
+                        <span>Your username name:{props.username}</span>
                         <br />
-                    </div>
-                </div>
-            </div>
-            <div class="row">
-                <div class="card" id="householdsList">
-                    <div class="card-title">
-                        Household Info
-                        </div>
-                    <div class="card-content">
-                        <span>Household ID:</span><span class="householdId"></span>
+                        <span>Your display name:{props.displayName}</span>
                         <br />
-                    </div>
-                    <div class="card-action">
-                        <button id="invite-user">Invite</button>
-                    </div>
-                    <div class="card-action">
-                        <button id="create-new">Create new household</button>
                     </div>
                 </div>
             </div>
--- client/src/pages/UserProfile.js
+++ client/src/pages/UserProfile.js
@@ -3,10 +3,28 @@ import API from "../utils/API";
 import Profile from "../components/Profile";
 
 class Main extends Component {
+
+    state = {
+        emailAddress: this.state.emailAddress,
+        username: this.state.username,
+        displayName: this.state.displayName,
+        householdID: this.state.householdID
+    };
+
     componentDidMount() { }
     render() {
         return (
-            <Profile />
+            <div>
+                <Profile
+                    emailAddress={this.state.emailAddress}
+                    username={this.state.username}
+                    displayName={this.state.displayName}
+                />
+                <br />
+                <InviteUserCard
+                    householdID={this.state.householdID}
+                />
+            </div>
         );
     }
 }
