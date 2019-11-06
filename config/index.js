1b7fac77b34ebac1c9f8e49b6ca1bdcc37961816
Colin Elving <viewtifuljho@hotmail.com>
2019-11-06 15:00:33 -0600

    updated user api and seeds

--- config/index.js
+++ config/index.js
@@ -3,9 +3,9 @@ mongoose.Promise = global.Promise;
 
 const uri =
   process.env.MONGODB_URI ||
-  "mongodb://user:apart1@ds141198.mlab.com:41198/heroku_9q7wwp10";
+  // "mongodb://user:apart1@ds141198.mlab.com:41198/heroku_9q7wwp10";
 
-// "mongodb://localhost/apartdb";
+"mongodb://localhost/apartdb";
 
 mongoose.connect(uri).then(
   () => {
