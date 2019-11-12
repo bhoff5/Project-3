b4e4edeb366a0dd4561048c43be83d3be21562c6
Shannon <shannonburke.art@gmail.com>
2019-11-11 23:41:22 -0600

    work on about us page

---
+++ client/src/pages/AboutUs.js
@@ -0,0 +1,22 @@
+import React, { Component } from "react";
+import API from "../utils/API";
+import { Redirect } from "react-router-dom";
+import Jumbotron from "../components/Jumbotron";
+import { brian } from "../images/brian";
+
+class Main extends Component {
+    render() {
+        <div>
+            <Jumbotron>
+                <h1>Meet the Team!</h1>
+            </Jumbotron>
+            <div className="row">
+                <div className="col m8 offset-m2">
+                    <img src={brian} className="team-photo" />
+                </div>
+            </div>
+        </div>
+    }
+}
+
+export default Main;
